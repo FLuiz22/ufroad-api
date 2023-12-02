ac3337fe5f5b585b50a6b6cc1a3601327b2c213d
maahog <guedesoliveira004@gmail.com>
2023-12-01 22:13:05 -0300

    feat: schema of course was created.

--- src/modules/Course/Course.js
+++ src/modules/Course/Course.js
@@ -1,9 +1,29 @@
 import { Schema, model } from "mongoose";
 
 const CourseSchema = new Schema ({
-
+    name: {
+        type: String,
+        required: true,
+    },
+    curriculum: [{
+        type: Schema.Types.ObjectId,
+        ref: "Curriculum",
+        required: true,
+        default: [],
+    }],
+    sclass: [{
+        type: Schema.Types.ObjectId,
+        ref: "SClass",
+        required: true,
+        default: [],
+    }],
+    minimumPeriods: {
+        type: Number,
+        required: true,
+        default: 9,
+    },
 });
 
-const Course = model("Course", UserSchema);
+const Course = model("Course", CourseSchema);
 
-export default User;
+export default Course;
