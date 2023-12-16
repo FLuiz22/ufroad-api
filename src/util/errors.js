0df975a0add12a0ce2a4c45d9ce2ad254a66ace9
FLuiz22 <filipeluiz39@gmail.com>
2023-12-16 20:26:34 -0300

    Feat: ErrorUnauthorized developed

--- src/util/errors.js
+++ src/util/errors.js
@@ -6,6 +6,11 @@ export class ErrorAlreadyExists extends Error {
     
 }
 
+/**
+ * Error used for when tries to access the system and haven't authorization.
+ */
 export class ErrorUnauthorized extends Error {
-    
+    constructor(message) {
+        super(message, 401);
+    }
 }
