e6d053d30179a526ed8d759f87338123f3bffcb4
FLuiz22 <filipeluiz39@gmail.com>
2023-12-16 16:32:57 -0300

    Feat: AuthService created

---
+++ src/modules/User/Auth/AuthService.js
@@ -0,0 +1,37 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import User from '../User.js';
+
+const signToken = user => {
+    return jwt.sign({
+        iss: 'UFRoad',
+        sub: user,
+        iat: new Date().getTime()
+    }, process.env.JWT_SECRET);
+};
+
+module.exports = {
+    async signIn(data) {
+        const { email, password } = data;
+
+        const user = await User.findOne({ email: email}).populate({path: 'course', select: 'name'});
+
+        if(!user) {
+            return { erro: 'Usuário ou senha incorreta!'};
+        }
+
+        const match = await bcrypt.compare(password, user.password);
+
+        if(!match) {
+            return { erro: 'Usuário ou senha incorreta!' };
+        }
+
+        delete user._doc.password;
+
+        const token = signToken(user);
+
+        return {
+            'user': user
+        };
+    }
+}
