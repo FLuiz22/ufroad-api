7dc25ad8b0d03f63261a90106b1df4c3d388a39b
FLuiz22 <filipeluiz39@gmail.com>
2023-12-16 17:10:02 -0300

    Feat: AuthRoutes created

---
+++ src/modules/User/Auth/AuthRoutes.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import AuthController from './AuthController.js';
+
+const router = new Router();
+
+router.post('signIn', AuthController.signIn);
+
+export default router;
