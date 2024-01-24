cb1aa091f5f7a7f2e9ab1f466e6bc40dac7a0aca
Mateus <130506942+mateusmf4@users.noreply.github.com>
2024-01-23 23:43:45 -0300

    fix: fix auth middleware not using async properly
    because of this, the errors thrown by this middleware were not caught
    by our error handler, crashing the application entirely.

--- src/middleware/auth/auth.js
+++ src/middleware/auth/auth.js
@@ -16,58 +16,73 @@ const authorize = async (req, res, next, type) => {
     if (scheme === null || !/^Bearer$/i.test(scheme))
         throw new ErrorUnauthorized("Token mal formatado.");
 
-    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
-        if (err) throw new ErrorUnauthorized("Token inválido.");
-    
-        const user = await User.findOne({ _id: decoded.sub });
-        //const course = await Course.findOne({ _id: decoded.sub }); [feat futura..]
-
-        switch(type) {
-
-            case "userExist":
-                if(!user){
-                    throw new ErrorNotFound("Usuário não existe");
-                }; break
-
-            case "itself":
-                if(!isAdmin(user) && (user._id.toString() !== req.params.id)){
-                    throw new ErrorUnauthorized("Usuário não autorizado");
-                }; break
-                
-            case "administrator":
-                if(!isAdmin(user)){ // se o usuario nao eh adm e tenta acessar algo de adm
-                    throw new ErrorUnauthorized("Usuário não autorizado"); 
-                }; break
-
-        /* feat futura...
-            case "authorized":
-                if(res.user.course !== course){ // se o curso do usuario é diferente do curso em questao
-                    throw new ErrorUnauthorized("Usuário não autorizado"); //  nao é do curso em questao e nao pode fazer determinadas coisas
-                }; break
-        */ 
-        }
-        next();
-    });
-};
-
-const isAdmin = (user) => user && user.isAdmin;
-
-export default {
-    userExist(req, res, next){
-        authorize(req, res, next, "userExist");
-    },
-    
-    isAdmin(req, res, next){
-        authorize(req, res, next, "administrator");
-    },
-
-    isItself(req, res, next){
-        authorize(req, res, next, "itself");
+    let decoded;
+    try {
+        // wrap into a promise, as jwt uses callbacks and those aren't catched by our error handler..
+        decoded = await new Promise((resolve, reject) => {
+            jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(decoded);
+                }
+            });
+        });
+    } catch (err) {
+        throw new ErrorUnauthorized("Token inválido.");
     }
 
-/* feat futura...
-    userAuthorized(req, res, next){
-        authorize(req, res, next, "authorized");
+    const user = await User.findOne({ _id: decoded.sub });
+    // const course = await Course.findOne({ _id: decoded.sub }); [feat futura..]
+
+    const isAdmin = (user) => user && user.isAdmin;
+
+    switch (type) {
+        case "userExist":
+            if (!user) {
+                throw new ErrorNotFound("Usuário não existe");
+            }
+            break;
+
+        case "itself":
+            if (!isAdmin(user) && user._id.toString() !== req.params.id) {
+                throw new ErrorUnauthorized("Usuário não autorizado");
+            }
+            break;
+
+        case "administrator":
+            if (!isAdmin(user)) {
+                // se o usuario nao eh adm e tenta acessar algo de adm
+                throw new ErrorUnauthorized("Usuário não autorizado");
+            }
+            break;
+
+        /* feat futura...
+        case "authorized":
+            if(res.user.course !== course){ // se o curso do usuario é diferente do curso em questao
+                throw new ErrorUnauthorized("Usuário não autorizado"); //  nao é do curso em questao e nao pode fazer determinadas coisas
+            }; break
+        */
+    }
+    next();
+};
+
+export default {
+    async userExist(req, res, next) {
+        return await authorize(req, res, next, "userExist");
+    },
+
+    async isAdmin(req, res, next) {
+        return await authorize(req, res, next, "administrator");
+    },
+
+    async isItself(req, res, next) {
+        return await authorize(req, res, next, "itself");
+    },
+
+    /* feat futura...
+    async userAuthorized(req, res, next){
+        return await authorize(req, res, next, "authorized");
     },
 */
 };
